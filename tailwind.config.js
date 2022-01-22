a1bcf88322980dd474ab0eedb734d03e2f37fdf7
hayoung0Lee <hayoung0.lee@gmail.com>
2022-01-22 16:41:09 +0900

    Seperate modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: "jit",
-  content: ["./src/contents/*.{html,js,ts,tsx}"],
+  content: ["./content-scripts/*.{html,js,ts,tsx}"],
   theme: {
     extend: {
       zIndex: {

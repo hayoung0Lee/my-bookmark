faa300f4213effa8dfa9a3ef64b58ac872652ef3
hayoung0Lee <hayoung0.lee@gmail.com>
2022-01-28 00:29:27 +0900

    Folder Style

--- packages/content-scripts/src/BoomarkFolder.tsx
+++ packages/content-scripts/src/BoomarkFolder.tsx
@@ -6,20 +6,29 @@ const BookmarkFolder = ({
 }: {
   bnode: chrome.bookmarks.BookmarkTreeNode;
 }) => {
+  // root
+  const isRoot = !bnode.parentId;
   return (
-    <div className="border-double border-4 border-indigo-600">
-      <div>
-        <IoFolderOutline /> {bnode.title}
+    <>
+      <div className="flex items-center text-lg">
+        {!isRoot && (
+          <>
+            <IoFolderOutline className="mx-1.5" />
+            {bnode.title || `BookmarksBar ${bnode.id}`}
+          </>
+        )}
       </div>
-      {bnode.children.map((bnode: chrome.bookmarks.BookmarkTreeNode) => {
-        const isFolder = bnode.children;
-        if (isFolder) {
-          return <BookmarkFolder key={bnode.id} bnode={bnode} />;
-        } else {
-          return <BookmarkNode key={bnode.id} bnode={bnode} />;
-        }
-      })}
-    </div>
+      <div className={isRoot ? `` : `pl-5`}>
+        {bnode.children.map((bnode: chrome.bookmarks.BookmarkTreeNode) => {
+          const isFolder = bnode.children;
+          if (isFolder) {
+            return <BookmarkFolder key={bnode.id} bnode={bnode} />;
+          } else {
+            return <BookmarkNode key={bnode.id} bnode={bnode} />;
+          }
+        })}
+      </div>
+    </>
   );
 };
 

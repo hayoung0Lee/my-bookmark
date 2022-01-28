a21ee92c97b818866cf413d4dda3600f08d5db27
hayoung0Lee <hayoung0.lee@gmail.com>
2022-01-29 02:07:26 +0900

    Update tailwind config

--- packages/content-scripts/tailwind.config.js
+++ packages/content-scripts/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: "jit",
-  content: ["./*.{html,js,tsx,ts}", "./src/*.{html,js,tsx,ts}"],
+  content: ["./*.{html,js,tsx,ts}", "./src/**/*.{html,js,tsx,ts}"],
   theme: {
     extend: { zIndex: { "top": "2147483646" } },
   },

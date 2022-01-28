2d549321b215dddf6a8fa2634982f267428f7401
hayoung0Lee <hayoung0.lee@gmail.com>
2022-01-29 01:22:30 +0900

    Button 오류 수정

--- packages/content-scripts/src/common/Button.tsx
+++ packages/content-scripts/src/common/Button.tsx
@@ -3,10 +3,15 @@ const Button = ({
   onClick,
 }: {
   children: JSX.Element | string;
-  onClick: (e?: MouseEvent) => void;
+  onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }) => {
   return (
-    <button className="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm mx-2">
+    <button
+      className={
+        "px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm mx-2"
+      }
+      onClick={onClick}
+    >
       {children}
     </button>
   );

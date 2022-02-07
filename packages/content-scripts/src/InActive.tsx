46cee36898ce4aa85e4f7d432ff69146e5a357a8
hayoung0Lee <hayoung0.lee@gmail.com>
2022-02-07 15:11:22 -0600

    Just little cleaning

--- packages/content-scripts/src/InActive.tsx
+++ packages/content-scripts/src/InActive.tsx
@@ -3,25 +3,25 @@ const InActive = ({ activate }: { activate: () => void }) => {
   return (
     <div
       className={`
-      fixed
-      right-0
-      w-[20px]
-      h-full
-      group`}
+        fixed
+        cursor-pointer
+        right-0
+        w-[20px]
+        h-full
+        group`}
       onClick={activate}
     >
       <div
         className={`
-      top-0
-      bottom-0
-      absolute
-      left-full
-      right-0
-      bg-slate-600
-      opacity-50
-      transition-left
-      duration-200
-      group-hover:left-0
+        bg-slate-600
+          opacity-50
+          h-full
+          absolute
+          left-full
+          right-0
+          transition-left
+          duration-200
+          group-hover:left-0
     `}
       >
       </div>

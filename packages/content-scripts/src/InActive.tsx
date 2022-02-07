c6020474115436adca7cf1852bbcad1843e13940
hayoung0Lee <hayoung0.lee@gmail.com>
2022-02-07 14:59:04 -0600

    Add trainsition to my hover bar

--- packages/content-scripts/src/InActive.tsx
+++ packages/content-scripts/src/InActive.tsx
@@ -1,9 +1,31 @@
+
 const InActive = ({ activate }: { activate: () => void }) => {
   return (
     <div
-      className={`fixed right-0 w-[20px] h-full bg-orange-400 opacity-20 hover:opacity-100`}
+      className={`
+      fixed
+      right-0
+      w-[20px]
+      h-full
+      group`}
       onClick={activate}
-    ></div>
+    >
+      <div
+        className={`
+      top-0
+      bottom-0
+      absolute
+      left-full
+      right-0
+      bg-slate-600
+      opacity-50
+      transition-left
+      duration-200
+      group-hover:left-0
+    `}
+      >
+      </div>
+    </div>
   );
 };
 
